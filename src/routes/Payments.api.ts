7267780781348fc87361f091ea7b9485360bea58
jieul2 <jieul2@kakao.com>
2026-03-19 13:10:56 +0900

    feat: #4 결제기능라우터 유저확인미들웨어 적용

--- src/routes/Payments.api.ts
+++ src/routes/Payments.api.ts
@@ -1,13 +1,14 @@
 import { Hono } from "hono";
 import paymentsController from "../controllers/payments.controller";
+import { authMiddleware } from "../middlewares/auth.middleware";
 
 const router = new Hono();
 
 // 결제 조회
-router.get("/", paymentsController.getPayments);
+router.get("/", authMiddleware, paymentsController.getPayments);
 // 결제 상태 업데이트
-router.patch("/:paymentId/status", paymentsController.updateStatus);
+router.patch("/:paymentId/status", authMiddleware, paymentsController.updateStatus);
 // 영수증 조회
-router.get("/:paymentId/receipt", paymentsController.getReceipt);
+router.get("/:paymentId/receipt", authMiddleware, paymentsController.getReceipt);
 
 export default router;
