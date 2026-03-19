00c940c84bbe9d933ec6431c21e47c6db88a5088
ming <ming92035@gmail.com>
2026-03-19 23:53:25 +0900

    feat: #5 bcrypt를 이용한 비밀번호 해싱 및 삭제 검증 로직 추가

--- src/routes/user.api.ts
+++ src/routes/user.api.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono";
 import User from "../models/User";
+import bcrypt from "bcrypt";
 import { userEmailSchema, updateUserSchema, validateBody } from "../middlewares/user.middleware";
 
 const userApi = new Hono();
@@ -19,14 +20,19 @@ userApi.get("/me", async (c) => {
 });
 
 // 프로필 정보 수정 (PUT /api/user/me?email=...)
-// TODO: 비밀번호 수정 시 해싱(Hashing) 로직 추가하기
 userApi.put("/me", validateBody(updateUserSchema), async (c) => {
   const email = userEmailSchema.parse(c.req.query("email"));
   const body = await c.req.json();
 
+  // 비밀번호가 본문에 포함되어 있다면 해싱하기
+  if (body.password) {
+    const saltRounds = 10;
+    body.password = await bcrypt.hash(body.password, saltRounds);
+  }
+
   const updatedUser = await User.findOneAndUpdate(
     { email },
-    body, // 검증된 바디 데이터 사용
+    body, 
     { new: true, runValidators: true }
   ).select("-password");
 
@@ -35,12 +41,26 @@ userApi.put("/me", validateBody(updateUserSchema), async (c) => {
 });
 
 // 회원 탈퇴 (DELETE /api/user/me?email=...)
-// TODO: 입력받은 password와 DB의 비밀번호 비교 로직 추가하기
 userApi.delete("/me", async (c) => {
   const email = userEmailSchema.parse(c.req.query("email"));
-  
-  const deletedUser = await User.findOneAndDelete({ email });
-  if (!deletedUser) return c.json({ message: "삭제할 사용자를 찾을 수 없습니다." }, 404);
+  const { password } = await c.req.json();
+
+  if (!password) {
+    return c.json({ message: "비밀번호를 입력해주세요." }, 400);
+  }
+
+  // 사용자 찾기 (비밀번호 검증을 위해 password 필드 포함)
+  const user = await User.findOne({ email });
+  if (!user) return c.json({ message: "사용자를 찾을 수 없습니다." }, 404);
+
+  // 비밀번호 비교하기 (입력된 평문 vs DB의 해싱된 암호)
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    return c.json({ message: "비밀번호가 일치하지 않습니다." }, 401);
+  }
+
+  // 일치할 경우 삭제 진행하기
+  await User.deleteOne({ email });
 
   return c.json({ message: "회원 탈퇴가 완료되었습니다." });
 });
